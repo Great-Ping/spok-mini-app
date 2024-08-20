7cfdf318521cdb1377e2e7992b220c2b7e9f1282
WithsCat <da18.06.03@yandex.ru>
2024-08-20 22:39:34 +0300

    Implemented settings page view

--- src/pages/settings/Settings.vue
+++ src/pages/settings/Settings.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
 import { PageHeader } from '@/shared/ui';
+import { ReferenceParameter, ToggleParameter } from '@/shared/ui/parameters';
+import { 
+  DeleteIcon,
+  NotificationsIcon,
+  ChatIcon,
+  ExitIcon,
+  StarIcon 
+} from '@/shared/svg';
+
 let header = "Настройки"
 
 </script>
 
 <template>
-  <main>
+  <main class="settings">
     <PageHeader style="text-align: center;">{{ header }}</PageHeader>
-    <
+    <ToggleParameter :icon="NotificationsIcon" name="Уведомления" onchange=""/>
+    <ReferenceParameter :icon="StarIcon" name="Оценить приложение" reference=""/>
+    <ReferenceParameter :icon="ChatIcon" name="Связь с разработчиком" reference=""/>
+    <ReferenceParameter :icon="ExitIcon" name="Выйти аккаунта" reference=""/>
+    <ReferenceParameter :icon="DeleteIcon" name="Удалить аккаунт" reference=""/>
   </main>
 </template>
 
